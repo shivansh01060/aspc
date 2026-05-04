b08d931def5bfa1854a0610b84f83417e1e5b25c
shivansh01060 <shivanshnarayan.sn@gmail.com>
2026-05-04 16:02:01 +0530

    Update App_final.jsx

--- src/App_final.jsx
+++ src/App_final.jsx
@@ -13,7 +13,6 @@ export default function App() {
     <div className="grain-overlay">
       <Navbar />
       <main>
-        {/* <HeroSection /> */}
         <VideoSection />
         <StorySection />
         <ProductSection />
